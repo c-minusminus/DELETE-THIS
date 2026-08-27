5138d228f0b1882654dd7bac4e3c5c41ea7b9f51
VoxelMaster64 <mcross.student@bccspa.org>
2026-08-27 17:39:20 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+//% color="#7C3AED" weight=100 icon="\uf1b2" block="THINGY"
+namespace TestDummy {
+    /** 
+     * Does a thing
+     */
+    //% group="Gameplay"
+    //% block="str %a"
+	export function functio(a: string) {
+		return a
+    }
+	/**
+     * Does a thing
+     */
+	//% group="Gameplay"
+    //% block="num %a"
+    export function f(a: number) {
+        return a
+    }
+}
+console.log(0)
